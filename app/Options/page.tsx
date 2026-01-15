85ae81b72875dbf3c30d60a7efeb6c8228e1c7f5
AdamKhelladi <khelladiadem5@gmail.com>
2026-01-15 17:01:00 +0100

    Add DropdownMenu component

--- app/Options/page.tsx
+++ app/Options/page.tsx
@@ -1,3 +1,4 @@
+import { title } from "process";
 import Container from "../Components/Container/page";
 
 import {
@@ -8,6 +9,24 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+
+const dropdownMenuItems = [
+  {
+    id: 1,
+    title: "SELECT CATEGORY",
+    options: ["Medicine", "Science", "Math"],
+  },
+  {
+    id: 2,
+    title: "SELECT LEVEL",
+    options: ["Beginner", "Intermediate", "Advanced"],
+  },
+  {
+    id: 3,
+    title: "SELECT TYPE",
+    options: ["Multiple Choice", "True/False"],
+  },
+];
 
 export default function page() {
   return (
@@ -34,7 +53,7 @@ export default function page() {
             <input
               type="number"
               id="number_of_questions"
-              className="outline-0 border-2 rounded-b-md w-full h-12 p-2 border-[#333] placeholder:text-[#777] bg-[#333] text-white focus:border-[#555]"
+              className="outline-0 border-2 rounded-xl duration-400 w-full h-12 p-2 border-[#333] placeholder:text-[#777] bg-[#333] text-white focus:border-[#555]"
               defaultValue={10}
               min={0}
               max={50}
@@ -43,17 +62,32 @@ export default function page() {
             />
           </div>
 
-          <DropdownMenu>
-            <DropdownMenuTrigger>Open</DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Profile</DropdownMenuItem>
-              <DropdownMenuItem>Billing</DropdownMenuItem>
-              <DropdownMenuItem>Team</DropdownMenuItem>
-              <DropdownMenuItem>Subscription</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+          <div className="border border-[#333] rounded-xl p-4 flex justify-evenly items-center">
+            {dropdownMenuItems.map((menu) => (
+              <DropdownMenu key={menu.id}>
+                <DropdownMenuTrigger className="outline-0 cursor-pointer bg-[#333] border border-[#333] rounded-lg px-4 py-3 text-white hover:bg-[#222] transition">
+                  {menu.title}
+                </DropdownMenuTrigger>
+
+                <DropdownMenuContent className="bg-[#2a2a2a] text-white border border-[#444] rounded-lg p-1 shadow-xl">
+                  <DropdownMenuLabel className="px-3 py-2 text-sm text-[#aaa]">
+                    {menu.title}
+                  </DropdownMenuLabel>
+
+                  <DropdownMenuSeparator className="bg-[#444]" />
+
+                  {menu.options.map((option, index) => (
+                    <DropdownMenuItem
+                      key={index}
+                      className="text-white px-4 py-2 cursor-pointer rounded-md hover:bg-[#444] focus:bg-[#444] data-[highlighted]:bg-[#444] data-[highlighted]:text-white"
+                    >
+                      {option}
+                    </DropdownMenuItem>
+                  ))}
+                </DropdownMenuContent>
+              </DropdownMenu>
+            ))}
+          </div>
         </div>
       </Container>
     </div>

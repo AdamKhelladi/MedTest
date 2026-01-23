8573df6cb9b7796e3a0f79757eb27e19f88bcd7d
AdamKhelladi <khelladiadem5@gmail.com>
2026-01-23 10:46:54 +0100

    Add Contact component

---
+++ app/Components/Contact.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Page() {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSend = () => {
+    setInputValue("");
+  };
+
+  return (
+    <div className="px-4 md:px-8 py-6 text-white rounded-xl bg-gradient-to-r from-cyan-900 to-purple-500 lg:flex lg:justify-between lg:items-center">
+      <div className="mb-8 lg:mb-0">
+        <h1 className="font-bold text-4xl mb-4 w-80">
+          Ready to Get Our New Stuff?
+        </h1>
+        <div className="bg-gray-50 p-2 rounded-4xl w-80">
+          <input
+            type="email"
+            placeholder="Your Email"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            className="w-50 mr-2 outline-none text-sky-900 p-2"
+          />
+          <button
+            type="button"
+            className="rounded-4xl px-7 py-2 cursor-pointer bg-gradient-to-r from-cyan-500 to-blue-600 duration-400 hover:bg-gradient-to-bl font-medium"
+            onClick={handleSend}
+          >
+            Send
+          </button>
+        </div>
+      </div>
+
+      <div className="lg:mt-auto">
+        <h3 className="font-bold text-xl mb-2">Need Help or Support?</h3>
+
+        <p className="text-gray-200 text-sm leading-relaxed max-w-sm">
+          We’re here to help with any questions about our products and services.
+          Contact us anytime.
+        </p>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,11 @@
-"use client";
-
 import Link from "next/link";
 import Header from "./Components/Header";
+import Categoreis from "./Components/Categories";
 import Image from "next/image";
 import Container from "./Components/Container/page";
-import Button from "./Components/Button/page";
-import { useRouter } from "next/navigation";
-
-const categories = [
-  {
-    name: "Medicine",
-    description: "Test your knowledge in various medical topics.",
-    imageUrl: "/images/medicineLogo.png",
-  },
-  {
-    name: "Pharmacy",
-    description: "Explore questions related to pharmaceuticals.",
-    imageUrl: "/images/phrmacyLogo.png",
-  },
-  {
-    name: "Dentistry",
-    description: "Challenge yourself with dental care questions.",
-    imageUrl: "/images/dentistLogo.png",
-  },
-];
+import Contact from "./Components/Contact";
 
 export default function Home() {
-  const router = useRouter();
-
   return (
     <div className="bg-gray-50 min-h-screen">
       <Header />
@@ -63,42 +41,9 @@ export default function Home() {
           </Link>
         </div>
 
-        <div className="py-20">
-          <h1 className="font-bold text-3xl lg:text-5xl text-sky-900 text-center">
-            Categories
-          </h1>
-          <p className="text-sm lg:text-lg text-gray-600 text-center">
-            Explore the available quiz categories
-          </p>
+        <Categoreis />
 
-          <div className="mt-10 grid grid-cols-1 place-items-center md:grid-cols-3 gap-8">
-            {categories.map((category: any, index: number) => (
-              <div
-                key={index}
-                className="min-h-[340px] flex flex-col items-center rounded-xl border-2 border-sky-600 p-4 duration-400 hover:shadow-lg hover:shadow-sky-600"
-              >
-                <Image
-                  className={index === 1 ? "w-32 mb-5" : ""}
-                  src={category.imageUrl}
-                  alt={category.name}
-                  width={100}
-                  height={200}
-                />
-
-                <h2 className="font-semibold text-2xl text-sky-900 mt-4">
-                  {category.name}
-                </h2>
-                <p className="text-gray-600 text-center mt-2 mb-6">
-                  {category.description}
-                </p>
-
-                <Button onClick={() => router.push("/Options")}>
-                  Start {category.name} MCQ
-                </Button>
-              </div>
-            ))}
-          </div>
-        </div>
+        <Contact />
       </Container>
     </div>
   );

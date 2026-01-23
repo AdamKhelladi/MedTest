dc2c699c4780c39877362665b358e6a0da04fc8a
AdamKhelladi <khelladiadem5@gmail.com>
2026-01-23 16:47:46 +0100

    Updates

--- _next/static/8wquUZJxEf2y_RcqE5IKa/_buildManifest.js
+++ _next/static/8wquUZJxEf2y_RcqE5IKa/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/MedTest//_next/:path+",
+        "destination": "/MedTest/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
